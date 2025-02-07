27068e984dcaea9f133bcc68b5aaafe4c13fb8c6
BugHunter-X <askzenai@gmail.com>
2025-02-08 00:54:04 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,7 @@
+```javascript
+<div class="flex">
+  <div class="bg-red-500 p-4 w-1/2"></div>
+  <div class="bg-blue-500 p-4 w-1/2"></div>
+</div>
+```
+This code snippet uses Tailwind's `flex` utility class to create a horizontal layout. However, if you don't set a width for the parent `div`, the child elements might not occupy the entire available width as intended.
